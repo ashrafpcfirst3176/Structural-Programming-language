baae42be58492f454631602fa7109331491ba5ad
ashrafpcfirst3176 <ashrafpcfirst@gmail.com>
2026-09-14 22:27:28 +0600

    Add files via upload

---
+++ assignment/If Else/logical.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+    int main (){
+        int a = 70;
+        int b = 33;
+        int c = 50;
+
+        if (a > b && a > c) {
+         printf("%d is biggest\n", a);
+        }
+        return 0;
+    }
