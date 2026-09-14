44af855de9518fb25c8a890fb88b26a4d3de776d
ashrafpcfirst3176 <ashrafpcfirst@gmail.com>
2026-09-14 20:47:45 +0600

    Add files via upload

---
+++ assignment/13_constants.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+    int main () {
+        const int roll = 54;
+      //     roll = 756; 
+        const float Cgpa = 3.80;
+    // Cgpa = 3.99;
+
+        printf("%d\n", roll);
+        printf("%f", Cgpa);
+        return 0;
+    }
